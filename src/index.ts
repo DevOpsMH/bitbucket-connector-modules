9d645375b8362b6b67be372200da1263a78c0544
boomerangll <yaronmironboomerangll@gmail.com>
2020-11-18 14:58:26 +0200

    kakatov

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,12 @@ const app = express();
 const {PORT = 3000} = process.env;
 
 app.get('/', (req: Request, res: Response) => {
+  console.log('kakatov');
   gavno.get({url: ''}, () => {});
   res.send({
-
     message: helloWorld(),
   });
 });
-
 
 app.listen(PORT, () => {
   console.log('server started at http://localhost:' + PORT);
